9c5174fa4740867bf2f1c568ed749269a5b7ee34
arturszolke <aszolke002@gmail.com>
2024-09-23 22:07:24 +0200

    Add keyboard navigation for subject input suggestions
    - Implement arrow key navigation through suggestion list
    - Allow selecting suggestion with Enter key
    - Close suggestion list with Escape key

--- src/components/SubjectInput.vue
+++ src/components/SubjectInput.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="position-relative">
         <AppInput :model-value="modelValue" @update:model-value="$emit('update:modelValue', $event)" type="text"
-            placeholder="E.g. Mathematics" label="Subject Name" @focus="showSuggestions = true"
-            @input="onSubjectInput" />
+            placeholder="E.g. Mathematics" label="Subject Name" @focus="showSuggestions = true" @input="onSubjectInput"
+            @keydown="handleKeyDown" />
         <ul v-if="showSuggestions && filteredSubjects.length > 0"
             class="list-group position-absolute w-100 suggestion-list" ref="suggestionList">
-            <li v-for="subject in filteredSubjects" :key="subject" class="list-group-item list-group-item-action"
+            <li v-for="(subject, index) in filteredSubjects" :key="subject"
+                class="list-group-item list-group-item-action" :class="{ 'active': index === selectedIndex }"
                 @click="selectSubject(subject)">
                 {{ subject }}
             </li>
@@ -27,6 +28,7 @@ const emit = defineEmits<{
 }>();
 
 const showSuggestions = ref(false);
+const selectedIndex = ref(-1);
 
 const filteredSubjects = computed(() => {
     if (!props.modelValue) return [];
@@ -39,11 +41,38 @@ const filteredSubjects = computed(() => {
 function selectSubject(subject: string) {
     emit('update:modelValue', subject);
     showSuggestions.value = false;
+    selectedIndex.value = -1;
 }
 
 function onSubjectInput(event: Event) {
     showSuggestions.value = true;
+    selectedIndex.value = -1;
     emit('update:modelValue', (event.target as HTMLInputElement).value);
+}
+
+function handleKeyDown(event: KeyboardEvent) {
+    if (!showSuggestions.value || filteredSubjects.value.length === 0) return;
+
+    switch (event.key) {
+        case 'ArrowDown':
+            event.preventDefault();
+            selectedIndex.value = (selectedIndex.value + 1) % filteredSubjects.value.length;
+            break;
+        case 'ArrowUp':
+            event.preventDefault();
+            selectedIndex.value = (selectedIndex.value - 1 + filteredSubjects.value.length) % filteredSubjects.value.length;
+            break;
+        case 'Enter':
+            event.preventDefault();
+            if (selectedIndex.value !== -1) {
+                selectSubject(filteredSubjects.value[selectedIndex.value]);
+            }
+            break;
+        case 'Escape':
+            showSuggestions.value = false;
+            selectedIndex.value = -1;
+            break;
+    }
 }
 </script>
 
